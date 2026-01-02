e18cfac9747466db091380bf3d28cbd7643c54e1
Karuppiah Natarajan <karuppiah.natarajan@capillarytech.com>
2026-01-02 16:32:48 +0530

    add fork bomb examples

---
+++ fork-bomb/fork-bomb.sh
@@ -0,0 +1,14 @@
+:(){ :|:& };:
+
+# : is a function name. : is a valid bash function name
+# :(){  } defines the function
+# inside the function :
+#   : is called and it's output is piped to : again, all in the background since we used &
+# ; is to end the function definition
+# : after the ; is the first invocation of the : function
+
+# We can write the above in other ways that are more readable since : is not a readable name
+
+bomb(){ bomb|bomb& };bomb
+
+bomb(){ bomb()|bomb()& };bomb()
